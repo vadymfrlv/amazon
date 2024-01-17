c9aa5a5a433e1b62c0b6f390d7dc544da7d26367
Vadym Frolov <vdfroloff@gmail.com>
2024-01-17 12:46:42 +0200

    feat: created mock data for tables

---
+++ src/mock/accounts.ts
@@ -0,0 +1,56 @@
+export const accountsData = [
+  {
+    accountId: 'acc-001',
+    email: 'user1@example.com',
+    authToken: 'token1',
+    creationDate: '2023-01-01',
+  },
+  {
+    accountId: 'acc-002',
+    email: 'user2@example.com',
+    authToken: 'token2',
+    creationDate: '2023-01-15',
+  },
+  {
+    accountId: 'acc-003',
+    email: 'user3@example.com',
+    authToken: 'token3',
+    creationDate: '2023-02-10',
+  },
+  {
+    accountId: 'acc-004',
+    email: 'user4@example.com',
+    authToken: 'token4',
+    creationDate: '2023-03-05',
+  },
+  {
+    accountId: 'acc-005',
+    email: 'user5@example.com',
+    authToken: 'token5',
+    creationDate: '2023-05-15',
+  },
+  {
+    accountId: 'acc-006',
+    email: 'user6@example.com',
+    authToken: 'token6',
+    creationDate: '2023-01-21',
+  },
+  {
+    accountId: 'acc-007',
+    email: 'user7@example.com',
+    authToken: 'token7',
+    creationDate: '2023-04-09',
+  },
+  {
+    accountId: 'acc-008',
+    email: 'user8@example.com',
+    authToken: 'token8',
+    creationDate: '2023-04-09',
+  },
+  {
+    accountId: 'acc-009',
+    email: 'user9@example.com',
+    authToken: 'token9',
+    creationDate: '2023-03-14',
+  },
+];
---
+++ src/mock/profiles.ts
@@ -0,0 +1,56 @@
+export const profilesData = [
+  {
+    profileId: 'prof-001',
+    accountId: 'acc-001',
+    country: 'USA',
+    marketplace: 'Amazon',
+  },
+  {
+    profileId: 'prof-002',
+    accountId: 'acc-002',
+    country: 'UK',
+    marketplace: 'eBay',
+  },
+  {
+    profileId: 'prof-003',
+    accountId: 'acc-003',
+    country: 'USA',
+    marketplace: 'eBay',
+  },
+  {
+    profileId: 'prof-004',
+    accountId: 'acc-004',
+    country: 'Germany',
+    marketplace: 'Amazon',
+  },
+  {
+    profileId: 'prof-005',
+    accountId: 'acc-005',
+    country: 'UK',
+    marketplace: 'eBay',
+  },
+  {
+    profileId: 'prof-006',
+    accountId: 'acc-006',
+    country: 'UK',
+    marketplace: 'Amazon',
+  },
+  {
+    profileId: 'prof-007',
+    accountId: 'acc-007',
+    country: 'Germany',
+    marketplace: 'eBay',
+  },
+  {
+    profileId: 'prof-008',
+    accountId: 'acc-008',
+    country: 'UK',
+    marketplace: 'eBay',
+  },
+  {
+    profileId: 'prof-009',
+    accountId: 'acc-009',
+    country: 'USA',
+    marketplace: 'eBay',
+  },
+];
