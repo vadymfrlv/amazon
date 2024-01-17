a16b893ef7bde01eafb4fbacd8a9a3607d9559ba
Vadym Frolov <vdfroloff@gmail.com>
2024-01-18 01:15:40 +0200

    feat: updated and styled back button

--- src/components/BackBtn/BackBtn.tsx
+++ src/components/BackBtn/BackBtn.tsx
@@ -1,12 +1,14 @@
 import { useNavigate } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 
+import s from './BackBtn.module.scss';
+
 export const BackButton = () => {
   const navigate = useNavigate();
 
   return (
-    <Button type="button" variant="outline-dark" onClick={() => navigate(-1)}>
-      Back
+    <Button className={s.backBtn} type="button" variant="dark" onClick={() => navigate(-1)}>
+      Go back
     </Button>
   );
 };
