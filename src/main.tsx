a385a5110c951368de70739f3c1dd6649d92d689
Vadym Frolov <vdfroloff@gmail.com>
2024-01-18 01:17:49 +0200

    chore: global style updates

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App.tsx';
 
-import 'styles/index.css';
+import 'styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
